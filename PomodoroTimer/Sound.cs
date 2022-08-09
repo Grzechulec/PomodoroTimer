1d53b3347a46721b5a880faa8f9273a33a62ee6e
grzegorzsmolak12 <grzegorzsmolak12@gmail.com>
2022-08-09 15:09:04 +0200

    Dodaj pliki projektów.

---
+++ PomodoroTimer/Sound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer
+{
+    public class Sound
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Source { get; set; } = null!;
+    }
+}
