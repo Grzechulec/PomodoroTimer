50326f0a7c416582ac9ac138f51be84886742342
grzegorzsmolak12 <grzegorzsmolak12@gmail.com>
2022-08-09 22:54:13 +0200

    something i did

---
+++ PomodoroTimer/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+        DbSet<Sound> Sounds { get; set; }
+        DbSet<Mode> Modes { get; set; }
+
+    }
+}
---
+++ PomodoroTimer/Mode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomodoroTimer
+{
+    class Mode
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public Sound workSound { get; set; }
+        public Sound changeSound { get; set; }
+        public Sound pauseSound { get; set; }
+        public int workDuration { get; set; }
+        public int pauseDuration { get; set; }
+        public int seriesAmount { get; set; }
+    }
+}
